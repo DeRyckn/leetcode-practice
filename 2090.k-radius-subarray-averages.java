5003b7b07a64401fa267e72f96ef5133281c4e83
Zirang Liu <derycknooo@gmail.com>
2025-09-05 15:47:17 -0400

    quick update

--- 2090.k-radius-subarray-averages.java
+++ 2090.k-radius-subarray-averages.java
@@ -7,7 +7,22 @@
 // @lc code=start
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        
+        int length = nums.length;
+        int[] average = new int[length];
+        long currentSum = 0;
+
+        for (int i = 0; i < length; i++) {
+            currentSum += nums[i];
+
+            if (i < k || i > length - k - 1) {
+                average[i] = -1;
+            }
+            if (i >= 2 * k) {
+                average[i - k] = (int)(currentSum / (k + k + 1));
+                currentSum -= nums[i - k - k];
+            }
+        }
+        return average;
     }
 }
 // @lc code=end
