f091c33bc738a00352ad9a8fba8f102f745db384
Zirang Liu <derycknooo@gmail.com>
2025-09-10 14:14:08 -0400

    quick update

---
+++ 1512.number-of-good-pairs/Hash Table Frequency Statistics.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1512 lang=java
+ *
+ * [1512] Number of Good Pairs
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int pairsCount = 0;
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        for (int freq : count.values()) {
+            pairsCount += freq * (freq - 1) / 2;
+        }
+        return pairsCount;
+    }
+}
+// @lc code=end
+
