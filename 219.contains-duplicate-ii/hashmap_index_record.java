c4f6d33bb82bef1aaa8f83817fca3a83f851d497
Zirang Liu <derycknooo@gmail.com>
2025-09-18 15:53:49 -0400

    quick update

---
+++ 219.contains-duplicate-ii/hashmap_index_record.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=219 lang=java
+ *
+ * [219] Contains Duplicate II
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
